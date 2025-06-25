385690224f005ecaad4b00ed986fb8fccf307cf3
theolukayodeasemudara <blaqnativity@gmail.com>
2025-06-25 11:07:52 +0100

    created MainLayout file so I can have multiple layouts for different screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import Navbar from "./components/Navbar";
 import HomeView from "./screens/HomeView";
 import ErrorPage from "./screens/ErrorPage";
 import Footer from "./components/Footer";
+import MainLayout from "./layouts/MainLayout";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="max-w-[1680px] mx-auto  px-4">
-      <Navbar />
       <Routes>
-        <Route path="/" element={<HomeView />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomeView />} />
+        </Route>
         <Route path="*" element={<ErrorPage />} />
       </Routes>
 
