0dca22119dfcc2418514e1b2bd8cbf8b30e66ca4
theolukayodeasemudara <blaqnativity@gmail.com>
2025-06-25 09:18:01 +0100

    reversed the dynamic link typing...will figure that out later. Also implemented a route catching that catches non-existent routes for the now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,31 +2,8 @@ import Logo from "../assets/logo.svg";
 import { Link } from "react-router-dom";
 import { navMenu } from "../../constants";
 import { RiMenuFill } from "@remixicon/react";
-import type { NavItem } from "../../constants";
 
 const Navbar = () => {
-  const renderLink = (link: NavItem): JSX.Element => {
-    const isExternal = link.url.startsWith("https://");
-
-    if (isExternal) {
-      return (
-        <a
-          href={link.url}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="custom-link"
-        >
-          {link.menu}
-        </a>
-      );
-    }
-
-    return (
-      <Link to={link.url} className="custom-link">
-        {link.menu}
-      </Link>
-    );
-  };
   return (
     <nav className="relative py-5 dark:bg-[var(--color-text)]">
       <div className="flex items-center justify-between">
@@ -36,7 +13,11 @@ const Navbar = () => {
 
         <ul className="hidden lg:flex font-medium space-x-8 items-center">
           {navMenu.map((link, index) => (
-            <li key={index}>{renderLink(link)}</li>
+            <li key={index}>
+              <Link to={link.url} className="custom-link">
+                {link.menu}
+              </Link>
+            </li>
           ))}
 
           <Link to="/" className="btnMain">
@@ -50,11 +31,15 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* mobile menu */}
+      {/* mobilemenu */}
       <div className="absolute top-10 right-0 block lg:hidden w-1/2 p-10 z-50 bg-[var(--color-body)]/95 rounded-bl-lg">
         <ul className="flex flex-col gap-4 items-start font-medium">
           {navMenu.map((link, index) => (
-            <li key={index}>{renderLink(link)}</li>
+            <li key={index}>
+              <Link to={link.url} className="custom-link">
+                {link.menu}
+              </Link>
+            </li>
           ))}
 
           <Link to="/" className="btnMain">
