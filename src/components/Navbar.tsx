26a1a201d55c1aa69aba7c461213b4ef2934a046
theolukayodeasemudara <blaqnativity@gmail.com>
2025-06-26 21:55:04 +0100

    mobile menu toggle fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import Logo from "../assets/logo.svg";
 import { Link } from "react-router-dom";
 import { navMenu } from "../../constants";
-import { RiMenuFill } from "@remixicon/react";
+import { RiMenuFill, RiCloseFill } from "@remixicon/react";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState<boolean>(false);
+
   return (
     <nav className="relative py-5 dark:bg-[var(--color-text)]">
       <div className="flex items-center justify-between">
@@ -11,6 +14,7 @@ const Navbar = () => {
           <img src={Logo} className="h-[1.25em] mr-3" alt="rootscards_logo" />
         </Link>
 
+        {/* Desktop Nav */}
         <ul className="hidden lg:flex font-medium space-x-8 items-center">
           {navMenu.map((link, index) => (
             <li key={index}>
@@ -19,34 +23,40 @@ const Navbar = () => {
               </Link>
             </li>
           ))}
-
           <Link to="/" className="btnMain">
             start free trial
           </Link>
         </ul>
 
-        {/* toggle Link */}
-        <div className="lg:hidden">
-          <RiMenuFill />
+        {/* Toggle Button */}
+        <div className="lg:hidden z-50" onClick={() => setMenu(!menu)}>
+          {menu ? <RiCloseFill size={24} /> : <RiMenuFill size={24} />}
         </div>
       </div>
 
-      {/* mobilemenu */}
-      <div className="absolute top-10 right-0 block lg:hidden w-1/2 p-10 z-50 bg-[var(--color-body)]/95 rounded-bl-lg">
-        <ul className="flex flex-col gap-4 items-start font-medium">
-          {navMenu.map((link, index) => (
-            <li key={index}>
-              <Link to={link.url} className="custom-link">
-                {link.menu}
-              </Link>
-            </li>
-          ))}
-
-          <Link to="/" className="btnMain">
-            start free trial
-          </Link>
-        </ul>
-      </div>
+      {/* Mobile Menu */}
+      {menu && (
+        <div className="absolute top-16 right-0 lg:hidden w-1/2 p-10 z-40 bg-[var(--color-body)]/95 rounded-bl-lg transition-all duration-300 ease-in-out">
+          <ul className="flex flex-col gap-4 items-start font-medium">
+            {navMenu.map((link, index) => {
+              return (
+                <li
+                  key={index}
+                  onClick={() => setMenu(false)}
+                  className="flex items-center gap-2"
+                >
+                  <Link to={link.url} className="custom-link">
+                    {link.menu}
+                  </Link>
+                </li>
+              );
+            })}
+            <Link to="/" className="btnMain" onClick={() => setMenu(false)}>
+              start free trial
+            </Link>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
