ae4bcc6f7bcffc6494276ddc7e6fc1fd1d1586d2
theolukayodeasemudara <blaqnativity@gmail.com>
2025-06-28 21:10:19 +0100

    changed the grid height for landing page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-8 h-full">
         {/* RootsCards Feature */}
         <div
-          className="relative p-5 md:p-10 flex flex-col gap-2 md:gap-4 justify-between md:justify-start h-[250px] md:h-full"
+          className="relative p-5 md:p-10 flex flex-col gap-2 md:gap-4 justify-between md:justify-start h-full rounded-2xl"
           style={{
             backgroundImage: `url(${gradient})`,
             backgroundSize: "cover",
@@ -32,7 +32,7 @@ const Hero = () => {
           <div className="flex">
             <div className="flex-1 space-y-1 md:space-y-4">
               <h2 className="text-lg md:text-3xl font-medium">RootsCards</h2>
-              <p className="text-md md:text-xl">
+              <p className="text-md md:text-xl font-normal">
                 Your smart card. Instantly connected.
               </p>
             </div>
@@ -48,7 +48,7 @@ const Hero = () => {
           <img
             src={card}
             alt="RootsCard"
-            className="h-[5rem] md:h-[8rem] lg:h-[10rem] absolute top-1/3 md:top-1/2 left-1/4 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/4"
+            className="h-[8rem] md:h-[12rem] md:absolute top-1/3 md:top-1/2 left-1/4 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/4"
           />
 
           <div className="w-fit">
@@ -62,7 +62,7 @@ const Hero = () => {
 
         {/* Rootscard Suite Feature */}
         <div
-          className="relative p-5 md:p-10 flex flex-col gap-2 md:gap-4 justify-between md:justify-start overflow-hidden h-[250px] md:h-[500px] lg:h-full"
+          className="relative p-5 md:p-10 flex flex-col gap-2 md:gap-4 justify-between md:justify-start overflow-hidden h-full  rounded-2xl"
           style={{
             backgroundImage: `url(${gradient})`,
             backgroundSize: "cover",
@@ -74,7 +74,9 @@ const Hero = () => {
               <h2 className="text-lg md:text-3xl font-medium">
                 Rootscard Suite
               </h2>
-              <p className="text-md md:text-xl">Your smart card + Helpdesk</p>
+              <p className="text-md md:text-xl font-normal">
+                Your smart card + Helpdesk
+              </p>
             </div>
             <div className="justify-end">
               <a href="" className="btnSmall">
@@ -88,7 +90,7 @@ const Hero = () => {
           <img
             src={dashboard}
             alt="RootsCard"
-            className="h-[10rem] md:h-[20rem] lg:h-[30rem] absolute bottom-0 md:-bottom-10 lg:-bottom-40 md:-right-30 -right-40"
+            className="h-[10rem] md:h-[20rem] lg:h-[30rem] md:absolute bottom-0 md:-bottom-10 lg:-bottom-40 md:-right-30 -right-40"
           />
 
           <div className="">
@@ -100,7 +102,6 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      ;
     </section>
   );
 };
